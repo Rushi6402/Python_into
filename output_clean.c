d04a8fa0ed6fe13328c2a083e6b2f13054cf7b62
Rushi patil <rushikeshpatil6402@gmail.com>
2025-11-21 22:58:43 +0530

    python file added

---
+++ output_clean.c
@@ -0,0 +1,16 @@
+I i'm bad boy you know
+i am really sorry
+so don't give up boy hii hello
+
+
+2025-11-21 10:15:23 INFO User logged in: john@example.com
+2025-11-21 10:16:10 ERROR Failed login attempt from user: mike@gmail.com
+2025-11-21 10:17:45 WARNING Disk space low
+2025-11-21 10:18:02 DEBUG Debug message for system check
+2025-11-21 10:19:33 CRITICAL System failure reported by admin@company.org
+2025-11-21 10:20:30 INFO User performed action
+
+ifo
+info
+INFO
+
